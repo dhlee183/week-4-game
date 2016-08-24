237f7333a6afc359f3e1b9ca05c4d788f2b41124
David Hyun Lee <David Hyun Lee>
2016-08-24 17:28:31 -0400

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,32 @@
 $(document).ready(function(){
 
+	var chosenCharacter;
+	var moveEnemy;
+
 	$(".character").on("click", function() {
 			
 		// Choose Character to Play With	
 		var chosenCharacter = $(this);
+			console.log(this);
 			$('#playerChar').append(chosenCharacter);
 
 		// Moves Remaining Characters to Choose Enemy Section	
 		var moveEnemy = $('.character').not(this);
-			moveEnemy.each(function(){
+			console.log(moveEnemy);
          	$("#chooseenemyChar").append(moveEnemy);
-     	});
 
-		
+         $(moveEnemy).on("click", function() {
+			// var chosenEnemy = moveEnemy;
+				chosenCharacter = moveEnemy.not(this);
+				$("#enemyChar").append(moveEnemy);
+		});	
 
-	});
+    });     	
+
+	// $(moveEnemy).on("click", function() {
+	// 	var chosenEnemy = $(this);
+	// 		console.log(this);
+	// 		$("#enemyChar").append(chosenEnemy);
+	// });
 
 });
