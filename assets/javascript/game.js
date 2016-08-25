0d37c3a4dbfd5275c79ec5ff25ef05b97b479f2d
David Hyun Lee <hyun183@Davids-MacBook-Pro.local>
2016-08-25 02:42:14 -0400

    added HP functionality to the js file and updated the html file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
 	var chosenCharacter;
 	var moveEnemy;
 	var chosenEnemy;
+	var chosenPlayerHP = 0;
+	var chosenEnemyHP = 0;
+	var random = Math.floor(Math.random() * 2);
 
 	$("button").on("click", function() {
 		if ($(this).hasClass("character")) {	
 		// Choose Character to Play With	
 		var chosenCharacter = $(this);
 			console.log(this);
-			$('#playerChar').append(chosenCharacter);	
-			$('#playerHP').html();
+			$('#playerChar').append(chosenCharacter);
+		var chosenPlayerHP = this.value;		
+			$('#playerHP').html("HP " + chosenPlayerHP);
 
 		// Moves Remaining Characters to Choose Enemy Section	
 		var moveEnemy = $('.character').not(this);
@@ -20,14 +24,22 @@ $(document).ready(function(){
         // Moves Chosen Enemy to the Enemy Character	
         moveEnemy.on("click", function() {
 			var chosenEnemy = $(this);
-			$("#enemyChar").append(chosenEnemy);
-			$('#playerChar').append(chosenCharacter);
+				$("#enemyChar").append(chosenEnemy);
+			var chosenEnemyHP = this.value;		
+				$('#enemyHP').html("HP " + chosenEnemyHP);
+				$('#playerChar').append(chosenCharacter);
 
 		});
 
     	}
 
     	if ($(this).hasClass("attack")) {
+    		chosenPlayerHP--;
+    		chosenPlayerHP = $('.character').value;
+    		$('#playerHP').html("HP " + chosenPlayerHP + random);
+    		chosenEnemyHP--;
+    		chooseEnemyHP = $('.character').value;
+    		$('#enemyHP').html("HP " + chosenEnemyHP + random);
 
     	}
 
